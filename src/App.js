199f516942708bc330a5d6e4359f07e2d70baa50
Your Name <abhishekkumar9608@gmail.com>
2022-10-20 15:40:31 +0530

    added api and table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState();
+  const [listData , setListData] = useState();
+
+  useEffect(() => {
+    fetch(
+      "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res && Object.values(res)[1]))
+      .catch((err) => console.log(err))
+  }, []);
+
+
+  useEffect(() => {
+  if(data){
+  const listValue = Object.keys(data).map((key) => ({...data[key], datetime: key}))
+  setListData(listValue)
+  }
+  },[data])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <th>DateTime </th>
+          <th>Open</th>
+          <th>High</th>
+          <th>Low</th>
+          <th>Close</th>
+          <th>Volume</th>
+        </tr>
+        {listData &&
+          listData.map((item) => {
+            return (
+              <tr>
+                <td>{item["datetime"]}</td>
+                <td>{item["2. high"]}</td>
+                <td>{item["2. high"]}</td>
+                <td>{item["3. low"]}</td>
+                <td>{item["4. close"]}</td>
+                <td>{item["5. volume"]}</td>
+              </tr>
+            );
+          })}
+      </table>
     </div>
   );
 }
